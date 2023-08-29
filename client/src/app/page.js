ae6a578e45dcaf395c4b9a270c3a70b03be5cbbe
BruceWu2001 <brucewuzijian@gmail.com>
2023-08-29 22:04:35 +0800

    initial setup with shadcn done go read readme in client folder

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import Navbar from './components/header/NavBar'
+// import Image from 'next/image'
+// import Navbar from './components/header/NavBar'
 
 export default function Home() {
   return (
