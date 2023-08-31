847c7119604703c291f3ec3d416174af5dd244e2
BruceWu2001 <brucewuzijian@gmail.com>
2023-08-31 15:25:13 +0800

    feat: added form validation, styling

--- client/src/components/pages/login/LoginForm.jsx
+++ client/src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,5 @@
 'use client'
-
+import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -13,6 +13,8 @@ import {
 import { Input } from "@/components/ui/input"
 import { toast } from "@/components/ui/use-toast"
 import { useForm, Controller } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { loginSchema } from "./loginSchema"
 
 const LoginForm = () => {
     const form = useForm({
@@ -20,58 +22,63 @@ const LoginForm = () => {
             email : "",
             password : "",
         },
+        resolver : yupResolver(loginSchema)
     })
 
     const {control ,formState: {errors} , handleSubmit, reset} = form;
 
-    const onSubmit = () => {
-         toast({
+    const onSubmit = (data) => {
+        console.log(data)
+        toast({
       title: "You submitted the following values:",
       description: (
         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          <code className="text-secondary bg-black">{JSON.stringify(data, null, 2)}</code>
         </pre>
       ),
     })
     }
     return (
     <div className="w-full drop-shadow-lg">
-        <div className="w-full bg-secondary flex justify-center px-8 rounded-t-lg">
+        <div className="w-full bg-secondary flex justify-center px-8 rounded-t-lg py-4">
             <h1 className="font-extrabold text-primary text-4xl">Tessera</h1>
         </div>
         <div className="bg-primary p-8 flex flex-col items-center rounded-b-lg">
             <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+                <form onSubmit={handleSubmit(onSubmit)} className="w-2/3 space-y-6">
                     <FormField
-                    control={form.control}
+                    control={control}
                     name="email"
                     render={({ field }) => (
                         <FormItem>
                         <FormLabel>Email</FormLabel>
                         <FormControl>
-                            <Input placeholder="" {...field} className="shadow-inner"/>
+                            <Input placeholder="" {...field} className="shadow-inner shadow-gray-500"/>
                         </FormControl>
-                        <FormMessage />
+                        <FormMessage className="text-red-500"/>
                         </FormItem>
                     )}
                     />
                     <FormField
-                    control={form.control}
-                    name="Password"
+                    control={control}
+                    name="password"
                     render={({ field }) => (
                         <FormItem>
                         <FormLabel>Password</FormLabel>
                         <FormControl>
-                            <Input placeholder="" {...field} />
+                            <Input placeholder="" {...field} className="shadow-inner shadow-gray-500"/>
                         </FormControl>
-                        <FormMessage />
+                        <FormMessage className="text-red-500"/>
                         </FormItem>
                     )}
                     />
                     <Button type="submit" className="w-full text-primary bg-secondary hover:bg-secondary">Sign In</Button>
                 </form>
             </Form>
-
+            <div className="flex py-8 gap-2">
+                <p>Don't have an account?</p> 
+                <Link href="/signup" className="underline text-cyan-600">Sign up</Link>
+            </div>
         </div>
     </div>);
 }
