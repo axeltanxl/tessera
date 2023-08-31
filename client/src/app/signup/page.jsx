6da19db3035e4daff1f87e1c5877123c961f9a42
BruceWu2001 <brucewuzijian@gmail.com>
2023-08-31 16:30:09 +0800

    feat: added 3d model, choose nicer looking model in future

--- client/src/app/signup/page.jsx
+++ client/src/app/signup/page.jsx
@@ -3,6 +3,7 @@ import SignUpForm from "@/components/pages/signup/SignUpForm";
 const Page = () => {
     return (
     <div className="w-full flex flex-col justify-center items-center">
+        
         <div className="w-2/5">
             <SignUpForm/>
         </div>
