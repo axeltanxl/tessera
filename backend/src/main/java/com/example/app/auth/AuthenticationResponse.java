2170cd43f026bf95d19149ed75249cbdf32ef1af
phosphurous <122954378+phosphurous@users.noreply.github.com>
2023-09-02 18:12:24 +0800

    fea: WIP <UserAuthentication>

---
+++ backend/src/main/java/com/example/app/auth/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.example.app.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+
+    // private final String error;
+}
