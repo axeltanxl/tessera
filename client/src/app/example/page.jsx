d507c3bcbff72285617491c0a82bb5be9bb669f5
BruceWu2001 <brucewuzijian@gmail.com>
2023-09-02 21:50:12 +0800

    feat: connecting to stripe

--- client/src/app/example/page.jsx
+++ client/src/app/example/page.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { loadStripe } from '@stripe/stripe-js';
 
 async function getData() {
     // You would usually fetch data from an API here.
@@ -12,16 +13,58 @@ async function getData() {
     // return res.json();
     return "Dashboard data";
   }
+
+  const storeItems = [
+    {
+        id : 1,
+        title : "item 1",
+        price : 0.01
+    },
+    {
+        id : 2,
+        title : "item 2",
+        price : 0.01
+    }
+]
+
+  const createSession = async(req, res) => {
+      const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+      try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types : ['card'],
+            mode : "payment",
+            line_items: req.body.items.map(item => {
+                const storeItem  = storeItems.get(item.id)
+                return {
+                    price_data : {
+                        currency : 'usd',
+                        product_data : {
+                            name : storeItem.name
+                        },
+                        unit_amount : storeItem.price
+                    },
+                    quantity : item.quantity
+                }
+            }),
+            success_url : "http://localhost:3000/checkout/success",
+            cancel_url : "http://localhost:3000/checkout/cancel",
+        })
+      } catch (error) {
+        
+      }
+  }
   
   export default async function Page() {
     const name = await getData();
-  
+
     return (
         <div>
             <Button aschild variant="outline" className="m-4 bg-example hover:bg-example text-black pointer-events:none">
             <Link href="/">Help</Link>
             </Button>
             <p>🤩 Hello {name}!</p>
+
+            <Button variant="outlined">checkout</Button>
         </div>
     )
   }
