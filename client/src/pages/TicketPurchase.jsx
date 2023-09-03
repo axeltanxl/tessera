bc49facf2b328316cdd0c8a1f9514ee1157d1ec5
alwaystryin88 <srneo.2022@scis.smu.edu.sg>
2023-09-03 16:32:21 +0800

    feat: selectable seating zones

---
+++ client/src/pages/TicketPurchase.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SeatingPlan from "./SeatingPlan";
+
+const TicketPurchase = () => {
+    return(
+        <div className="min-h screen ">
+            <SeatingPlan />
+        </div>   
+    )
+}
+
+export default TicketPurchase;
