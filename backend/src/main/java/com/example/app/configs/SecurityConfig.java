f04510ddb9d9ec665790631179c2b15f661c5b44
phosphurous <122954378+phosphurous@users.noreply.github.com>
2023-09-05 17:28:53 +0800

    fix: CORS error

--- backend/src/main/java/com/example/app/configs/SecurityConfig.java
+++ backend/src/main/java/com/example/app/configs/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
 		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
