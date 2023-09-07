b85dce9b9f99f62bcd6ffe951208b69397b53b0f
BruceWu2001 <zijian.wu.2022@scis.smu.edu.sg>
2023-09-07 14:13:11 +0800

    feat: added cancel page

--- client/src/app/paymentFeedback/cancel/page.jsx
+++ client/src/app/paymentFeedback/cancel/page.jsx
@@ -1,6 +1,23 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 const CancelPage = () => {
-    return <div>
-        cancel page
+    return <div className="w-full flex flex-col justify-center items-center gap-8">
+        <div className="flex flex-col justify-center items-center gap-4 drop-shadow-xl p-12">
+            <div className="flex flex-col justify-center items-center gap-2">
+                <Image width={200} height={200} src="/images/error.svg"/>
+                <p className="text-4xl">Purchase Cancelled</p>
+            </div>
+            <div className="flex gap-4">
+                <Button variant="outlined"  className="text-primary bg-secondary ">
+                    Return to Home
+                </Button>
+
+                <Button variant="outlined"  className="text-primary bg-secondary ">
+                    View Purchase History
+                </Button>
+            </div>
+        </div>
     </div>
 }
 
