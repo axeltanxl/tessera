84882a0abb04832e9c104c7528550e68dc0fee1a
phosphurous <122954378+phosphurous@users.noreply.github.com>
2023-09-14 15:04:26 +0800

    feat: View and Update User Details

---
+++ backend/src/main/java/com/example/app/models/UserDTO.java
@@ -0,0 +1,23 @@
+package com.example.app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserDTO {
+    private Long userID;
+    private String username;
+    private int contactNum;
+    private String address;
+
+
+}
