5d12a50a03e767c47d89e753e3c1ff61acdbfd08
BruceWu2001 <zijian.wu.2022@scis.smu.edu.sg>
2023-09-14 22:26:38 +0800

    chore: incl message for products page as sample

--- client/src/app/products/page.jsx
+++ client/src/app/products/page.jsx
@@ -22,7 +22,9 @@ const Products = () => {
     return (
         <div className="flex flex-col gap-4">
             <p className="font-bold text-lg">products page temp</p>
-    
+            <p>if nothing here check if you have events in your db</p>
+{/* sample data */}
+{/* INSERT INTO event VALUES (3, '2023-08-19', 24, '2023-08-20', 1, 1, "concert", "description", '{"catA" : 100,"catB" : 110,"catC" : 120,"catD" : 130}'); */}
             {prices && prices.map((price) => {
                 console.log(price.pricePerCategory)
                 return (
