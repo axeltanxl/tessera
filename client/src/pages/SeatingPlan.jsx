acab0d0ef7f78f9a464fd3172276eff10a3953a2
alwaystryin88 <srneo.2022@scis.smu.edu.sg>
2023-09-14 12:44:04 +0800

    feat: display selected seating category and selectable quantity

--- client/src/pages/SeatingPlan.jsx
+++ client/src/pages/SeatingPlan.jsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
 
-export default function SeatingPlan () {
-    const [selectedZone, setSelectedZone] = useState();
+const SeatingPlan = ({setSelectedZone,selectedZone,setSelectedCat,selectedCat,setSelectedPrice,selectedPrice}) => {
+
 
     const seatingPlan = [
-        // { category: "CAT 1", zones: ["PA1", "PA2", "PB1", "PB2", "PB3", "PB4",
-        //                             "PC1", "PC2", "PC3", "PC4", "PD1", "PD2", "PD3", "PD4",
-        //                             "PE2", "PE3", "PF1", "PF2", "PF3", "PF4", "PF5", "PF6", "PF7",
-        //                             "112", "113", "114", "115", "116", "117",
-        //                             "134", "135", "136", "137", "138", "139"] },
-        { category: "CAT 1", zones: [
+
+        { category: "CAT 1", price: 348, zones: [
             {zoneId: "PA1", style: { top:"100%",left:"22%",height:"70%",width:"8%",backgroundColor:"#FFDB0E" }},
             {zoneId: "PA2", style: { top:"100%",right:"22%",height:"70%",width:"8%",backgroundColor:"#FFDB0E" }},
             {zoneId: "PB1", style: { top:"100%",left:"22%",height:"70%",width:"12%",marginTop:"4%",backgroundColor:"#FFDB0E" }},
@@ -34,7 +30,7 @@ export default function SeatingPlan () {
             {zoneId: "PG4", style: { top:"100%",right:"30%",height:"100%",width:"12%",marginTop:"25%",backgroundColor:"#FFDB0E" }},
             {zoneId: "PG5", style: { top:"100%",right:"30%",height:"100%",width:"12%",marginTop:"30%",backgroundColor:"#FFDB0E" }},
         ] },
-        { category: "CAT 2", zones: [
+        { category: "CAT 2", price: 328, zones: [
             {zoneId: "PH1", style: { top:"100%",left:"25%",height:"100%",width:"10%",marginTop:"36%",backgroundColor:"#68CDFF" }},
             {zoneId: "PH2", style: { top:"100%",left:"35%",height:"100%",width:"10%",marginTop:"36%",backgroundColor:"#68CDFF" }},
             {zoneId: "PH3", style: { top:"100%",left:"45%",height:"100%",width:"10%",marginTop:"36%",backgroundColor:"#68CDFF" }},
@@ -47,7 +43,7 @@ export default function SeatingPlan () {
             {zoneId: "100", style: { top:"100%",left:"40%",height:"100%",width:"10%",marginTop:"42%",backgroundColor:"#68CDFF" }},
             {zoneId: "150", style: { top:"100%",right:"40%",height:"100%",width:"10%",marginTop:"42%",backgroundColor:"#68CDFF" }},
         ] },
-        { category: "CAT 3", zones: [
+        { category: "CAT 3", price: 288, zones: [
             {zoneId: "200", style: { top:"100%",left:"40%",height:"100%",width:"10%",marginTop:"48%",backgroundColor:"#FF6A68" }},
             {zoneId: "250", style: { top:"100%",right:"40%",height:"100%",width:"10%",marginTop:"48%",backgroundColor:"#FF6A68" }},
             {zoneId: "106", style: { top:"100%",right:"15%",height:"100%",width:"8%",marginTop:"18%",backgroundColor:"#FF6A68" }},
@@ -59,7 +55,7 @@ export default function SeatingPlan () {
             {zoneId: "158", style: { top:"100%",left:"15%",height:"100%",width:"6%",marginTop:"6%",backgroundColor:"#FF6A68" }},
             {zoneId: "159", style: { top:"100%",left:"15%",height:"100%",width:"6%",backgroundColor:"#FF6A68" }},
         ] },
-        { category: "CAT 4", zones: [
+        { category: "CAT 4", price: 248, zones: [
             {zoneId: "152", style: { top:"100%",left:"15%",height:"100%",width:"8%",marginTop:"42%",backgroundColor:"#68FFAC" }},
             {zoneId: "153", style: { top:"100%",left:"15%",height:"100%",width:"8%",marginTop:"36%",backgroundColor:"#68FFAC" }},
             {zoneId: "154", style: { top:"100%",left:"15%",height:"100%",width:"8%",marginTop:"30%",backgroundColor:"#68FFAC" }},
@@ -69,7 +65,7 @@ export default function SeatingPlan () {
             {zoneId: "104", style: { top:"100%",right:"15%",height:"100%",width:"8%",marginTop:"30%",backgroundColor:"#68FFAC" }},
             {zoneId: "105", style: { top:"100%",right:"15%",height:"100%",width:"8%",marginTop:"24%",backgroundColor:"#68FFAC" }},
         ] },
-        { category: "CAT 5", zones: [
+        { category: "CAT 5", price: 168, zones: [
             {zoneId: "252", style: { top:"100%",left:"8%",height:"100%",width:"6%",marginTop:"42%",backgroundColor:"#C168FF" }},
             {zoneId: "253", style: { top:"100%",left:"8%",height:"100%",width:"6%",marginTop:"36%",backgroundColor:"#C168FF" }},
             {zoneId: "254", style: { top:"100%",left:"8%",height:"100%",width:"6%",marginTop:"30%",backgroundColor:"#C168FF" }},
@@ -79,7 +75,7 @@ export default function SeatingPlan () {
             {zoneId: "204", style: { top:"100%",right:"8%",height:"100%",width:"6%",marginTop:"30%",backgroundColor:"#C168FF" }},
             {zoneId: "205", style: { top:"100%",right:"8%",height:"100%",width:"6%",marginTop:"24%",backgroundColor:"#C168FF" }},
         ] },
-        { category: "CAT 6", zones: [
+        { category: "CAT 6", price: 108, zones: [
             {zoneId: "206", style: { top:"100%",right:"8%",height:"100%",width:"6%",marginTop:"18%",backgroundColor:"#FFAF68"}},
             {zoneId: "207", style: { top:"100%",right:"8%",height:"100%",width:"6%",marginTop:"12%",backgroundColor:"#FFAF68" }},
             {zoneId: "208", style: { top:"100%",right:"8%",height:"100%",width:"6%",marginTop:"6%",backgroundColor:"#FFAF68" }},
@@ -97,16 +93,19 @@ export default function SeatingPlan () {
         setSelectedZone(zoneId);
     };
 
+    const selectPrice = (price) => {
+      setSelectedPrice(price);
+    }
+
+    const selectCat = (cat) => {
+      setSelectedCat(cat);
+    }
+
+
     return (
         <div>
             <h1 className="text-center">Seating Plan</h1><br/>
             <div className="stage">STAGE</div>
-            <div className="first-path"></div>
-            <div className="center-stage"></div>
-            <div className="second-path"></div>
-            <div className="outer-stage"></div>
-            <div className="left-pole"></div>
-            <div className="right-pole"></div>
             <div className="seating-plan">
                 {seatingPlan.map((category, categoryIndex) => (
                     <div key={categoryIndex}>
@@ -114,9 +113,10 @@ export default function SeatingPlan () {
                             {category.zones.map((zone, zoneIndex) => (
                                 <div
                                     key={zoneIndex} className={`zone ${selectedZone === zone.zoneId ? "selected" : ""}`}
-                                    style={zone.style} onClick={() => selectZone(zone.zoneId)}>
-                                        {zone.zoneId}
+                                    style={zone.style} onClick={ () => {selectZone(zone.zoneId);selectCat(category);selectPrice(category.price);} }>
+                                      {zone.zoneId}
                                 </div>
+                                
                             ))}
                         </div>
                     </div>
@@ -126,10 +126,11 @@ export default function SeatingPlan () {
 
             <style jsx>{`
                 .stage {
-                    position: absolute;
+                    position: relative;
                     align-items: center;
                     justify-content: center;
-                    font-size: 14px;
+                    font-size: 16px;
+                    padding: 2%;
                     display: flex;
                     height: 5%;
                     width: 34%;
@@ -163,12 +164,12 @@ export default function SeatingPlan () {
                 }
                 @media (max-width: 768px) {
                     .seating-plan {
-                        margin: 10px;
-                        padding: 10px;
+                        margin: 5px;
+                        padding: 5px;
                     }
 
                     .zone {
-                        font-size: 8px;
+                        font-size: 6px;
                     }
                 }
                 @media (min-width: 768px) {
@@ -178,10 +179,15 @@ export default function SeatingPlan () {
                     }
 
                     .zone {
-                        font-size: 12px;    
+                        font-size: 12px;
                     }
                 }
             `}</style>
+            
+
         </div>
+
     )
 }
+
+export default SeatingPlan;
