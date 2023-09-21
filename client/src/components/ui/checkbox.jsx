16cd08f4876b15ae12864ea8e87f091bc86d5e47
Olivia <oliviaow.2022@scis.smu.edu.sg>
2023-09-21 13:14:56 +0800

    feat: ticketcard

--- client/src/components/ui/checkbox.jsx
+++ client/src/components/ui/checkbox.jsx
@@ -6,8 +6,16 @@ import { Check } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
-const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
+const Checkbox = React.forwardRef(({handleSelect, className, ...props }, ref) => {
+  const [isChecked, setIsChecked] = React.useState(false);
+  const handleCheckboxChange = () => {
+    // toggle the state when the checkbox is clicked
+    setIsChecked(!isChecked);
+    handleSelect(isChecked);
+  };
+  return (
   <CheckboxPrimitive.Root
+    onChange={handleCheckboxChange}
     ref={ref}
     className={cn(
       "peer h-4 w-4 shrink-0 rounded ml-2 border border-[#B4C1DB] ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
@@ -18,7 +26,7 @@ const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
       <Check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
-))
+)})
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
 export { Checkbox }
