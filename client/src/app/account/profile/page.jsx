6bd19bf1ebeb223f4565717d68a0ff75a9bcdc74
olivia <111635889+oliviaow2022@users.noreply.github.com>
2023-09-26 10:35:29 +0800

    feat: getEvents

--- client/src/app/account/profile/page.jsx
+++ client/src/app/account/profile/page.jsx
@@ -1,18 +1,29 @@
 'use client'
-import React from 'react'
-import SideNav from '@/components/ui/accountNav/SideNav'
+import { Separator } from "@/components/ui/separator"
+import SignUpForm from "@/components/pages/signup/SignUpForm"
+import SideNav from "@/components/ui/accountNav/SideNav"
+const Profile = () => {
+    return (
+        <section className='flex mt-10'>
+            <div className='mr-20 ml-10'>
+                <SideNav activeTab={3} />
+            </div>
+            <div className="flex flex-1 flex-col space-y-6">
+                <div>
+                    <h3 className="text-lg font-medium">Profile</h3>
+                    <p className="text-sm text-muted-foreground">
+                        Update details about your profile here
+                    </p>
+                </div>
+                <Separator />
+                <div className="w-full flex flex-col">
+                    <div className="w-3/5 bg-primary p-8 flex flex-col items-center rounded-b-lg">
+                        <SignUpForm actionName={"Update Profile"} />
+                    </div>
+                </div>
+            </div>
+        </section>
 
-const MyProfile = () => {
-  return (
-    <div className='flex '>
-            <div className='mr-10'>
-                <SideNav activeTab={3}/>
-            </div>
-            <div>
-                <div>My Profile</div>
-            </div>
-        </div>
-  )
+    )
 }
-
-export default MyProfile
+export default Profile;
