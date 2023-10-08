ba78100095ded72dd2740af6306b62801ed72b3c
olivia <111635889+oliviaow2022@users.noreply.github.com>
2023-10-08 22:46:01 +0800

    feat: small ui changes

--- client/src/app/account/resell-tickets/page.jsx
+++ client/src/app/account/resell-tickets/page.jsx
@@ -48,7 +48,7 @@ const ResellTickets = () => {
                                 </TableCell>
                                 <TableCell>
                                     <button onClick={handleOpen} className='bg-amber-300 rounded-sm px-4 py-1 ml-4 text-sm'>Set price</button>
-                                    <button className='bg-amber-300 rounded-sm px-4 py-1 ml-4 text-sm'>Remove</button>
+                                    <button className='bg-black text-white rounded-sm px-4 py-1 ml-4 text-sm'>Remove</button>
                                 </TableCell>
                                 <Modal
                                     open={open}
@@ -56,9 +56,9 @@ const ResellTickets = () => {
                                     aria-labelledby="modal-modal-title"
                                     aria-describedby="modal-modal-description"
                                 >
-                                    <div className='absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] w-[500px] h-[300px] bg-white rounded-sm'>
+                                    <div className='absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] w-[500px] h-[350px] bg-white rounded-sm'>
                                         <div className="flex flex-col flex-1 p-10">
-                                            <div className='grid grid-cols-2 w-[500px]'>
+                                            <div className='grid grid-cols-2 w-[450px]'>
                                                 <div className='flex justify-center flex-col w-3/4'>
                                                     <p className='font-bold'>Taylor Swift The Eras Tour in Singapore </p>
                                                     <p className='flex items-center'><CalendarIcon />8 March 2024</p>
@@ -68,14 +68,16 @@ const ResellTickets = () => {
                                                     <TicketCard category={"A"} section={"PA"} row={"18"} seatNo={"22"} />
                                                 </div>
                                             </div>
+                                            <p className='mt-1 font-bold'>Original price: $200</p>
                                             <div className='flex mt-4'>
-                                                Input resale price
-                                                <input type="price" placeholder='Resale Price in SGD' className='ml-6 border border-[#B4C1DB] rounded-sm px-2'>
+                                                <p className='font-bold'>Input resale price:</p>
+                                                <p className='ml-4'>$</p>
+                                                <input type="price" placeholder='e.g. 200' className='border border-[#B4C1DB] rounded-sm px-2'>
                                                 </input>
                                             </div>
 
                                         </div>
-                                        <div className='flex justify-center mt-2'>
+                                        <div className='flex justify-center mt-1'>
                                             <button className='border border-amber-300 rounded-sm px-4 py-1 mr-4 text-sm' onClick={handleClose}>Cancel</button>
                                             <button className='bg-amber-300 rounded-sm px-4 py-1 ml-4 text-sm'>Resell </button>
                                         </div>
@@ -92,7 +94,7 @@ const ResellTickets = () => {
                                 </TableCell>
                                 <TableCell>
                                     <button className='bg-amber-300 rounded-sm px-4 py-1 ml-4 text-sm'>Set price</button>
-                                    <button className='bg-amber-300 rounded-sm px-4 py-1 ml-4 text-sm'>Remove</button>
+                                    <button className='bg-black text-white rounded-sm px-4 py-1 ml-4 text-sm'>Remove</button>
                                 </TableCell>
 
                             </TableRow>
