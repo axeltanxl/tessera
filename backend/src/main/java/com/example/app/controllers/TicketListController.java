422e51ba0030e4aec4032cf7fc9b93567d3b7430
phosphurous <122954378+phosphurous@users.noreply.github.com>
2023-10-13 08:09:50 +0800

    refractor: swapped back

--- backend/src/main/java/com/example/app/controllers/TicketListController.java
+++ backend/src/main/java/com/example/app/controllers/TicketListController.java
@@ -37,7 +37,7 @@ public class TicketListController {
     @Autowired
     private RunRepository runRepo;
 
-    //For general public who wants to see each listing.
+    // For general public who wants to see each listing.
     @GetMapping("ticketListings/{listingID}")
     public ResponseEntity<Object> getListingByTicketListID(@PathVariable long listingID) {
 
@@ -48,7 +48,7 @@ public class TicketListController {
         return ResponseEntity.ok(eachTicket);
     }
 
-    //For the public who wants to see all the listings
+    // For the public who wants to see all the listings
     @GetMapping("ticketListings")
     public ResponseEntity<List<TicketListing>> getAllListings() {
         List<TicketListing> ticketLists = ticketListRepo.findAll();
@@ -63,25 +63,79 @@ public class TicketListController {
     // private UserRepository userRepo;
 
     // @GetMapping("ticketListing/{listingID}")
-    // public ResponseEntity<Object> getListingByTicketListID(@RequestHeader("Authorization") String authorizationHeader, 
+    // public ResponseEntity<Object>
+    // getListingByTicketListID(@RequestHeader("Authorization") String
+    // authorizationHeader,
     // @PathVariable long listingID) {
-        
-    //     final String TOKEN = authorizationHeader.replace("Bearer ", ""); // Remove "Bearer " prefix
-        
-    //     final ModelMapper modelMapper = new ModelMapper();
-    //     String getCurrEmail = midWare.extractUsername(TOKEN);
 
-    //     User user = userRepo.findByEmail(getCurrEmail);
+    // final String TOKEN = authorizationHeader.replace("Bearer ", ""); // Remove
+    // "Bearer " prefix
 
-    //     // Convert User entity to UserDTO, excluding the password
-    //     UserDTO userObj = modelMapper.map(user, UserDTO.class);
+    // final ModelMapper modelMapper = new ModelMapper();
+    // String getCurrEmail = midWare.extractUsername(TOKEN);
 
-    //     Optional<TicketListing> ticketList = ticketListRepo.findById(listingID);
-    //     if (!ticketList.isPresent() || ticketList.get().getUser().getUserID() != userObj.getUserID()) {
-    //         return ResponseEntity.notFound().build();
-    //     }
+    // User user = userRepo.findByEmail(getCurrEmail);
 
-    //     // String getCurrListing = 
-    //     return ResponseEntity.ok(ticketList);
+    // // Convert User entity to UserDTO, excluding the password
+    // UserDTO userObj = modelMapper.map(user, UserDTO.class);
+
+    // Optional<TicketListing> ticketList = ticketListRepo.findById(listingID);
+    // if (!ticketList.isPresent() || ticketList.get().getUser().getUserID() !=
+    // userObj.getUserID()) {
+    // return ResponseEntity.notFound().build();
     // }
+
+    // // String getCurrListing =
+    // return ResponseEntity.ok(ticketList);
+    // }
+
+    // For the public who wants to see all the listings for 1 event
+    @GetMapping("events/{eventID}/ticketListings")
+    public ResponseEntity<List<TicketListingWithSeat>> getAllListingsByEventID(@PathVariable long eventID) {
+
+        List<TicketListing> ticketListsByEventID = ticketListRepo.findAllByEventEventID(eventID);
+
+        if (ticketListsByEventID.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // Create a map to group TicketListings by listingID
+        Map<Long, TicketListingWithSeat> ticketListingsMap = new HashMap<>();
+
+        for (TicketListing ticketListing : ticketListsByEventID) {
+            TicketListingWithSeat ticketListingWithSeat = new TicketListingWithSeat();
+            ticketListingWithSeat.setTicketListing(ticketListing);
+
+            // Fetch and associate the Seat with the TicketListing
+            Long ticketID = ticketListing.getTicket().getTicketID();
+            Optional<Ticket> optTicketObj = ticketRepo.findById(ticketID);
+
+            if (optTicketObj.isPresent()) {
+                Ticket ticket = optTicketObj.get();
+                Long seatID = ticket.getSeat().getSeatID();
+                Optional<Seat> optSeatObj = seatRepo.findById(seatID);
+
+                if (optSeatObj.isPresent()) {
+                    Seat seat = optSeatObj.get();
+                    ticketListingWithSeat.setSeat(seat);
+                }
+            }
+
+            // Fetch and associate the Run with the TicketListing
+            Long runID = ticketListing.getRun().getRunID();
+            Optional<Run> optRunObj = runRepo.findById(runID);
+            if (optRunObj.isPresent()) {
+                Run run = optRunObj.get();
+                ticketListingWithSeat.setRun(run);
+            }
+
+            // Add the TicketListingWithSeat to the map
+            ticketListingsMap.put(ticketListing.getListingID(), ticketListingWithSeat);
+        }
+
+        // Convert values of the map (TicketListingWithSeat objects) into a list
+        List<TicketListingWithSeat> ticketListsWithSeats = new ArrayList<>(ticketListingsMap.values());
+
+        return ResponseEntity.ok(ticketListsWithSeats);
+    }
 }
