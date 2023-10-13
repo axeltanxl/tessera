4c6bcb59a13a278d46ad3762a9361e1ea9719c6e
Axel Tan <axeltanxl@gmail.com>
2023-10-13 13:52:58 +0800

    feat: return event info when fetching open marketplaces

--- backend/src/main/java/com/example/app/controllers/MarketplaceController.java
+++ backend/src/main/java/com/example/app/controllers/MarketplaceController.java
@@ -1,18 +1,19 @@
 package com.example.app.controllers;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.app.models.Event;
+import com.example.app.models.EventDTO;
 import com.example.app.models.Marketplace;
 import com.example.app.repositories.MarketplaceRepository;
-import com.example.app.repositories.RunRepository;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,6 +26,13 @@ public class MarketplaceController {
   public ResponseEntity<List<Marketplace>> getAllOpenMarketplaces() {
 
     List<Marketplace> marketplaces = marketplaceRepository.findByStatus("open");
+
+    final ModelMapper modelMapper = new ModelMapper();
+    for (Marketplace marketplace : marketplaces) {
+      Event event = marketplace.getRun().getEvent();
+      EventDTO eventDTO = modelMapper.map(event, EventDTO.class);
+      marketplace.setEventDTO(eventDTO);
+    }
     
     return ResponseEntity.ok(marketplaces);
   }
