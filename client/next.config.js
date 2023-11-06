630b47b84bb47e12fcd5fb0be4de5db16178ad58
Axel Tan <axeltanxl@gmail.com>
2023-11-06 18:37:46 +0800

    fix: changed next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
     experimental: {
         serverActions: true
       },
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, './src'),
+      };
+  
+      return config;
+    },
 }
 
 module.exports = nextConfig
