2ce3b41fe9ccc35c5ae5534b93a22ea4ad85fded
BruceWu2001 <zijian.wu.2022@scis.smu.edu.sg>
2023-11-07 16:25:47 +0800

    fix: ticketing

--- client/src/app/api/ticketing/route.js
+++ client/src/app/api/ticketing/route.js
@@ -1,4 +1,7 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import { getServerSession } from 'next-auth'
+import { PrismaClient } from "@prisma/client";
+
 // import CryptoJS from 'crypto-js'
 
 // export async function POST(request){
@@ -12,3 +15,53 @@
 //     return NextResponse.json({message : "decrypted"}, { status: 201});
 
 // }
+
+export async function GET(request){
+    const authsession =  await getServerSession();
+    console.log("authsession!!!", authsession);
+    const email = authsession.user.email;
+    console.log(email);
+
+    const prisma = new PrismaClient();
+
+    // get user ID
+    const { userID } = await prisma.user.findFirst({
+        where : {
+            email : email 
+        },
+        select : {
+            userID : true,
+        }
+    })
+
+    const tickets = await prisma.transaction.findMany({
+        where : {
+            buyerID : userID,
+        },
+        select : {
+            ticketID : true,
+        }
+    })
+    
+    // await prisma.ticket.findMany({
+    //     where : {
+    //         userID : userID
+    //     },
+    //     select : {
+    //         ticketID : true,
+    //     }
+    // })
+
+    // const ticketsFromMarketPlace = [];
+
+    // for (let ticket of tickets){
+    //     const id = ticket.ticketID
+    //     console.log(id)
+        
+    //     // await prisma.ticket.findUnique({})
+    // }
+
+    console.log(tickets);
+    return NextResponse.json({tickets : JSON.stringify(tickets , (key, value) => {return typeof value === 'bigint' ? value.toString() : value;})}, { status: 200});
+}
+
